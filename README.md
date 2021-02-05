c6c774ce83476595d62711d66b859885d8aa624c
ASUS-ПК\ASUS <aniasatryan336@gmail.com>
2021-02-05 12:35:14 +0400

    Create to-do list

---
+++ README.md
@@ -0,0 +1,11 @@
+# To-do List
+
+This projects helps to organise tasks you need to complete, or things that you want to do. 
+
+## Content 
+
+* JS
+* SASS
+* CSS
+* HTML
+ 
--- js/toDoList.js
+++ js/toDoList.js
@@ -1,1 +1,38 @@
-toDoList.js
+var addToDoButton = document.getElementsByClassName("addToDo");
+
+function createOwnElement(tag, options) {
+	var element = document.createElement(tag);
+
+	if (options.classNames) {
+		for (var i = 0; i < options.classNames.length; i++) {
+			element.classList.add(options.classNames[i]);
+		}
+	}
+
+	return element;
+}
+
+for (var i = 0; i < addToDoButton.length; i++) {
+	addToDoButton[i].addEventListener('click', function(event) {
+		var element = createOwnElement("p", {classNames: ["paragraph"]});
+		var inputField = event.target.previousSibling;
+		var toDoContainer = event.target.nextElementSibling;
+
+		element.innerText = inputField.value;
+	    toDoContainer.appendChild(element);
+	    inputField.value = "";
+
+	    element.addEventListener('click', function() {
+			if(element.style.textDecoration === "line-through") {
+				element.style.textDecoration = "none";
+				
+			} else {
+				element.style.textDecoration = "line-through";
+			}
+		});
+
+		element.addEventListener('dblclick', function() {
+			toDoContainer.removeChild(element);
+		});
+	}); 
+}
